f497da90feb9ff77af41d6fa72a5bd87feede1e1
patassetbuilders <pjames@assetbuilders.com.au>
2014-10-31 21:21:59 +1000

    lesson2.3 Post Controller and restful actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,45 @@
 class PostsController < ApplicationController
+  
   def index
-
+    @posts = Post.all
+  end
+    
+  def show
+      @post = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_strong_params)
+    if @post.save
+      
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @post =Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_strong_params)
+      redirect_to posts_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+  end
+  
+  private
+  def post_strong_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end
--- db/migrate/20141028120010_create_user.rb
+++ db/migrate/20141028120010_create_user.rb
@@ -2,6 +2,7 @@ class CreateUser < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
+      t.timestamps
     end
   end
 end
