a43b5f0e6c10dcaad29aebe3116e675709305206
patassetbuilders <pjames@assetbuilders.com.au>
2014-11-05 06:50:40 +1000

    adding date and url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def fix_url(url)
+   if url.starts_with?('http://www')
+       url
+     elsif  url.starts_with?('www')
+        "http://#{url}"
+     elsif  url.starts_with?('https:') 
+       url
+     else 
+        "http://www.#{url}"
+    end
+  end
+  
+  def ozdate(date)
+    date.strftime("%A, %e %b %H:%M")
+  end
+  
 end
