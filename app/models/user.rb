ac5f38b6194265b9c8c4e7ae70171a43ba84c9ad
patassetbuilders <pjames@assetbuilders.com.au>
2014-11-08 07:05:38 +1000

    adding validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 5}
+  
   has_secure_password validations: false
 end
