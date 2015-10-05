870b1d3cc0f3feda851a2af8f82cf26ef19b569d
patassetbuilders <pjames@assetbuilders.com.au>
2015-10-05 19:58:23 +1000

    implementing role to post controller
    only post creator or admin can edit the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:show, :index]
+  before_action :require_creator, only: [:edit, :update]
   
   
   def index
@@ -27,7 +29,7 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @post =Post.find(params[:id])
+    @post =Post.find_by(slug: params[:id])
   end
   
   def update
@@ -64,4 +66,13 @@ class PostsController < ApplicationController
   def post_strong_params
     params.require(:post).permit(:title, :url, :description, category_ids:[])
   end
+  
+  def set_post
+    @post = Post.find_by slug: params[:id]
+  end
+  
+  def require_creator
+    access_denied unless logged_in? and (current_user == @post.user|| current_user.admin?)
+  end
+
 end
